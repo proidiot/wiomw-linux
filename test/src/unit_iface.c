b8a926e8d95af63d1cdff3c4f5dcb32c1811fbeb
Charles Southerland <csoutherland@io3o.com>
2014-10-07 17:54:57 -0500

    Added test_print_iface_data_tracker

--- test/src/unit_iface.c
+++ test/src/unit_iface.c
@@ -327,6 +327,105 @@ void test_print_iface_diff()
 	}
 }
 
+void test_print_iface_data_tracker()
+{
+	note("running test_print_iface_data_tracker");
+
+	struct iface_history_data* actual_hdata;
+	struct iface_nohistory_data* actual_nhdata;
+	struct data_tracker* tracker;
+
+	struct nlmsghdr* nlh = NULL;
+	struct ifinfomsg* ifi = NULL;
+	unsigned char buf[MNL_SOCKET_BUFFER_SIZE];
+	unsigned int i = 0;
+	const unsigned char family = AF_UNSPEC;
+	const int ifindex = 1;
+	const unsigned short hwtype = ARPHRD_ETHER;
+	const unsigned int ifi_flags = IFF_UP | IFF_RUNNING | IFF_DYNAMIC;
+	const unsigned int ifi_change = 0xFFFFFFFF;
+	const char* qdsp = "pfifo_fast";
+	const char* name = "eth0";
+	const uint32_t mtu = 1500;
+	const int link = ifindex;
+	const unsigned char mac[6] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB};
+	const unsigned char bmac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+	srandom(time(NULL));
+	for (i = 0; i < MNL_SOCKET_BUFFER_SIZE; i++) {
+		buf[i] = (unsigned char)(((unsigned long int)random()) % 8);
+	}
+	nlh = mnl_nlmsg_put_header(buf);
+	nlh->nlmsg_type = RTM_GETLINK;
+	nlh->nlmsg_flags = 0;
+	ifi = mnl_nlmsg_put_extra_header(nlh, sizeof(struct ifinfomsg));
+	ifi->ifi_family = family;
+	ifi->ifi_index = ifindex;
+	ifi->ifi_type = hwtype;
+	ifi->ifi_flags = ifi_flags;
+	ifi->ifi_change = ifi_change;
+	if (!mnl_attr_put_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_BROADCAST, 6, bmac)) {
+		fail("unable to put ifla_broadcast");
+	}
+	if (!mnl_attr_put_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_ADDRESS, 6, mac)) {
+		fail("unable to put ifla_address");
+	}
+	if (!mnl_attr_put_strz_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_IFNAME, name)) {
+		fail("unable to put ifla_ifname");
+	}
+	if (!mnl_attr_put_u32_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_MTU, mtu)) {
+		fail("unable to put ifla_mtu");
+	}
+	if (!mnl_attr_put_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_LINK, sizeof(int), &link)) {
+		fail("unable to put ifla_link");
+	}
+	if (!mnl_attr_put_strz_check(nlh, MNL_SOCKET_BUFFER_SIZE, IFLA_QDISC, qdsp)) {
+		fail("unable to put ifla_qdisc");
+	}
+
+	if ((tracker = prepare_data_tracker(iface_data_size, nlh, &get_iface_header_cb, &get_iface_attr_cb)) == NULL) {
+		fail("prepare_data_tracker returned NULL");
+	} else {
+		FILE* stream = tmpfile();
+
+		const char* expected = "{\"family\":\"AF_UNSPEC\",\"type\":"
+			"\"ARPHRD_ETHER\",\"up\":1,\"running\":1,"
+			"\"dynamic\":1,\"volatile\":1,\"mac\":"
+			"\"01:23:45:67:89:AB\",\"bcast_mac\":"
+			"\"FF:FF:FF:FF:FF:FF\",\"mtu\":1500,\"real_iface\":"
+			"\"eth0\",\"qdsp\":\"pfifo_fast\",\"name\":\"eth0\","
+			"\"blacklisted\":0,\"rx_packets\":0,\"tx_packets\":0,"
+			"\"rx_bytes\":0,\"tx_bytes\":0,\"rx_errors\":0,"
+			"\"tx_errors\":0,\"rx_dropped\":0,\"tx_dropped\":0,"
+			"\"multicast\":0,\"collisions\":0,"
+			"\"rx_length_errors\":0,\"rx_over_errors\":0,"
+			"\"rx_crc_errors\":0,\"rx_frame_errors\":0,"
+			"\"rx_fifo_errors\":0,\"rx_missed_errors\":0,"
+			"\"tx_aborted_errors\":0,\"tx_carrier_errors\":0,"
+			"\"tx_fifo_errors\":0,\"tx_heartbeat_errors\":0,"
+			"\"tx_window_errors\":0,\"rx_compressed\":0,"
+			"\"tx_compressed\":0,\"last_changed\":";
+		char actual[BUFSIZ];
+
+		print_data_tracker(stream, tracker, &print_iface, &print_iface_diff, NULL);
+
+		rewind(stream);
+
+		if (fgets(actual, BUFSIZ, stream) == NULL) {
+			fail("unable to fgets");
+		}
+
+		fclose(stream);
+
+		if (strncmp(expected, actual, strnlen(expected, BUFSIZ)) != 0) {
+			fail("print_iface_data_tracker did not match");
+			printf("expected: %s\n", expected);
+			printf("actual: %s\n", actual);
+		} else {
+			pass("print_iface_data_tracker matched");
+		}
+	}
+}
+
 int main()
 {
 	set_configuration(0, NULL);
@@ -338,6 +437,8 @@ int main()
 	test_print_iface();
 
 	test_print_iface_diff();
+
+	test_print_iface_data_tracker();
 
 	return 0;
 }
